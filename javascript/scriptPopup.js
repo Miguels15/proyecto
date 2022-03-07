01096f18cdbd4b93a81d6b15f9a4c63bbb145410
Stevenhzw <100164210+Stevenhzw@users.noreply.github.com>
2022-03-07 01:32:19 -0600

    Update scriptPopup.js

--- javascript/scriptPopup.js
+++ javascript/scriptPopup.js
@@ -1,12 +1,15 @@
+//declarar las variables y los elementos que tomaran del codigo html
 var btnOpenPopup = document.getElementById('popup-open'),
     overlay = document.getElementById('overlay'),
         popup = document.getElementById('popup'),
         btnClosePopup = document.getElementById('btn-cerrar-popup');
 
+//agregar la clase "active" cuando se haga click en el boton de comprar juego
 btnOpenPopup.addEventListener('click', function(){
     overlay.classList.add('active');
     popup.classList.add('active');
         
+//quitar la clase active cuando se haga click en el icono de "X" de cerrar pop up    
 });
 btnClosePopup.addEventListener('click', function(){
     overlay.classList.remove('active');
